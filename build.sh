f11af294dd59b5ee17e170ccf7eb65fcb1e0efac
Victor Olvera <dudeofx@gmx.com>
2021-07-01 12:42:44 -0400

    bug fixes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-emcc rotating_galaxy.c -s USE_WEBGL2=1 -s ALLOW_MEMORY_GROWTH=1 --preload-file assets --use-preload-plugins -o index.js
+emcc rotating_galaxy.c -s USE_WEBGL2=1 -s --preload-file assets --use-preload-plugins -o index.js
 
 
 
